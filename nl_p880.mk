45884dbfe9e90db9b311d55dbf05aa3c336d3fb2
laufersteppenwolf <laufersteppenwolf@gmail.com>
2014-01-26 21:18:11 +0100

    Add NamelessROM support

--- nl_p880.mk
+++ nl_p880.mk
@@ -1,6 +1,3 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/telephony.mk)
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
@@ -8,22 +5,21 @@ TARGET_SCREEN_WIDTH := 720
 # Release name
 PRODUCT_RELEASE_NAME := p880
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# This device has NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/nameless/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/p880/p880.mk)
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := Optimus 4X HD
-#PRODUCT_VERSION_DEVICE_SPECIFIC :=
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p880
-PRODUCT_NAME := cm_p880
+PRODUCT_NAME := nl_p880
 PRODUCT_BRAND := LG
 PRODUCT_MODEL := LG-P880
 PRODUCT_MANUFACTURER := LGE
@@ -32,3 +28,6 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_PACKAGES += Torch
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x3 BUILD_FINGERPRINT="lge/x3/p880:4.4/KRT16S/920375:user/release-keys" PRIVATE_BUILD_DESC="x3-user 4.4 KRT16S 920375 release-keys"
+
+# Inherit the SIM Toolkit
+PRODUCT_PACKAGES += Stk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,5 @@
 # limitations under the License.
 #
 
-add_lunch_combo cm_p880-userdebug
+add_lunch_combo nl_p880-userdebug
+add_lunch_combo nl_p880-eng
