a835656a155045c8e9892859649de5c443df6c8b
Jake Whatley <jacob.whatley91@gmail.com>
2014-02-07 01:04:59 -0500

    move to new vendor config
    Change-Id: If501efedf9b156bfd1d9317fbff3da2a05b3b268

--- nameless_p880.mk
+++ nameless_p880.mk
@@ -6,13 +6,7 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_RELEASE_NAME := p880
 
 # APNs
-$(call inherit-product, vendor/nameless/config/apns.mk)
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/nameless/config/common.mk)
+$(call inherit-product, vendor/nameless/config/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/p880/p880.mk)
