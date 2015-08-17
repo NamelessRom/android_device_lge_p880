c39cc1a1c6af275e356571df4445f1a080091f1a
laufersteppenwolf <laufersteppenwolf@gmail.com>
2015-08-17 23:43:14 +0200

    P880: Don't copy FFMPEG codecs
    Change-Id: Ie2ca9cc43a3fea8e349440aa36826488f9db5bb8

--- p880.mk
+++ p880.mk
@@ -52,7 +52,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/asound.conf:system/etc/asound.conf \
